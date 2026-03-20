e8021e26046d96f4f99c49f3e0cb5a22e7c43b86
Marco Antonio Gonzalez Junior <m@rco.sh>
2026-03-20 16:14:27 -0300

    Modify vscode.sh to streamline installation process
    Updated zypper install command to include --no-confirm and --no-recommends options.
    
    Signed-off-by: Marco Antonio Gonzalez Junior <m@rco.sh>

--- apps/tumbleweed/vscode.sh
+++ apps/tumbleweed/vscode.sh
@@ -2,4 +2,4 @@
 
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc &&
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/zypp/repos.d/vscode.repo > /dev/null
-sudo zypper install code
+sudo zypper install --no-confirm --no-recommends code
