96086ead9ffa42a1462cd6682ebec3f149ca8553
Marco Antonio Gonzalez Junior <m@rco.sh>
2026-03-20 16:13:11 -0300

    Add installation script for Visual Studio Code
    Signed-off-by: Marco Antonio Gonzalez Junior <m@rco.sh>

---
+++ apps/tumbleweed/vscode.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc &&
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/zypp/repos.d/vscode.repo > /dev/null
+sudo zypper install code
